28ef99d450fb9b6322b282cf33863d84a48556a3
kristina nommensen <jonas.nommensen@gmail.com>
2020-10-07 02:40:05 +0200

    Queue/Repeat System V.0.2

--- commands/Queue.cmd.js
+++ commands/Queue.cmd.js
@@ -1,11 +1,20 @@
+const { MessageEmbed } = require("discord.js")
 
 module.exports.run = (client, message, args) => {
 
+    const array = client.queue[message.channel.guild.id].array
+    let embed = new MessageEmbed()
+    embed.setTitle('Queue')
 
+    for (let i = 0; i < array.lenght; i++) {
+        embed.addField('#' + String(i), `[${array[i].info.title}](${array[i].link})`)
+    }
+
+    message.channel.send(embed)
 
 }
 
 module.exports.help = {
-    name: "...",
+    name: "queue",
     description: "..."
 }
